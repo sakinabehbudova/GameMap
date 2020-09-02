c36a54e6f2550cac364d1451255a2aae9bf63f41
Sakina <sakina.behbudova1996@gmail.com>
2020-09-02 22:50:24 +0400

    The button to scrool top of the page

---
+++ js/app.js
@@ -0,0 +1,62 @@
+const backToTopButton = document.getElementById('back-to-top-btn');
+
+window.addEventListener('scroll', scrollFunction);
+
+function scrollFunction() {
+    if (window.pageYOffset > 300) {   //show backToTopButton
+        if (!backToTopButton.classList.contains('btnEntrance')) {
+            backToTopButton.classList.remove('btnExit');
+            backToTopButton.classList.add('btnEntrance');
+            backToTopButton.style.display = "block";
+        }
+    }
+    else {    // Hide backToTopButton
+        if (backToTopButton.classList.contains('btnEntrance')) {
+            backToTopButton.classList.remove('btnEntrance');
+            backToTopButton.classList.add('btnExit');
+            setTimeout(function () {
+                backToTopButton.style.display = "none";
+            }, 300)
+        }
+    }
+}
+
+backToTopButton.addEventListener("click", smoothScroll);
+
+//function backToTop() {
+//    window.scrollTo(0, 0); }
+
+
+
+function smoothScroll() {
+    const targetPoition = 0;
+    const startPosition = window.pageYOffset;
+    const distance = targetPoition - startPosition;
+    const duration = 1200;
+    let start = null;
+
+    window.requestAnimationFrame(step);
+
+    function step(timestamp) {
+        if (!start) start = timestamp;
+        const progress = timestamp - start;
+        window.scrollTo(0, easeInOutCubic(progress, startPosition, distance, duration));
+        if (progress < duration) window.requestAnimationFrame(step);
+
+    }
+
+}
+function easeInOutCubic(t, b, c, d) {
+    t /= d / 2;
+    if (t < 1) return c / 2 * t * t * t + b;
+    t -= 2;
+    return c / 2 * (t * t * t + 2) + b;
+};
+
+
+
+
+
+
+
+
